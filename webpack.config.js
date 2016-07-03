04523324c24502e47da646baa922839f6cbfed32
Shawn Rodriguez <shawn.w.rodriguez@gmail.com>
2016-07-03 15:00:17 -0700

    add babel loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,17 @@ module.exports = {
   output: {
     path: `${__dirname}/public`,
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
   }
 };

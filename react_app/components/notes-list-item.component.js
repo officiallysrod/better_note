8bc7470ddcc12aba3889c9ce2ecb4726405f2a27
Shawn Rodriguez <shawn.w.rodriguez@gmail.com>
2016-07-10 17:59:42 -0700

    better formatting for the text displayed in the notes list

--- react_app/components/notes-list-item.component.js
+++ react_app/components/notes-list-item.component.js
@@ -12,15 +12,44 @@ class NotesListItem extends React.Component {
     this.props.toggleActiveNote(this.props.note);
   }
 
+  stripMarkdownCharacters(markdownString) {
+    let output = markdownString;
+    output = output
+      .replace(/\n={2,}/g, '\n')                             // Header
+      .replace(/~~/g, '')                                    // Strikethrough
+      .replace(/(- \[ \])/g, '')                             // Checkboxes
+      .replace(/`{3}.*\n/g, '')                              // Fenced codeblocks
+      .replace(/<(.*?)>/g, '$1')                             // Remove HTML tags
+      .replace(/^[=\-]{2,}\s*$/g, '')                        // Remove setext-style headers
+      .replace(/\[\^.+?\](: .*?$)?/g, '')                    // Remove footnotes
+      .replace(/\s{0,2}\[.*?\]: .*?$/g, '')
+      .replace(/!\[.*?\][\[\(].*?[\]\)]/g, '')               // Remove images
+      .replace(/\[(.*?)\][\[\(].*?[\]\)]/g, '$1')            // Remove inline links
+      .replace(/>/g, '')                                     // Remove Blockquotes
+      .replace(/^\s{1,2}\[(.*?)\]: (\S+)( ".*?")?\s*$/g, '') // Remove reference-style links?
+      .replace(/^#{1,6}\s*([^#]*)\s*(#{1,6})?/gm, '$1')      // Remove atx-style headers
+      .replace(/([\*_]{1,3})(\S.*?\S)\1/g, '$2')
+      .replace(/(`{3,})(.*?)\1/gm, '$2')
+      .replace(/^-{3,}\s*$/g, '')
+      .replace(/`(.+?)`/g, '$1')
+      .replace(/\n{2,}/g, '\n\n');
+    return output;
+  }
+
   render() {
     const listItemClass = classNames({
       'list-group-item': true,
       active: this.props.activeState
     });
 
+    const cleanString = this.stripMarkdownCharacters(this.props.body);
+    const [firstLine, ...bodyArray] = cleanString.split('\n');
+    const body = bodyArray.join(' ');
+
     return (
       <li className={listItemClass} onClick={this.handleClick}>
-        {this.props.body}
+        <p><strong>{firstLine}</strong></p>
+        <p>{body}</p>
       </li>
     );
   }
